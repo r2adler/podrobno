a31b84be70b5ebfde9718c883a685e7d37ca57c3
Artur Khareksian <r2mail1122@gmail.com>
2023-03-02 12:03:36 +0300

    'commit-1'

---
+++ src/components/Accordion/Accordion.tsx
@@ -0,0 +1,37 @@
+import {FC} from "react";
+
+interface AccordionProps {
+    titleValue: string
+    collapsed: boolean
+}
+
+export const Accordion: FC<AccordionProps> = (props) => {
+
+    return (
+        <>
+            <AccordionTitle title={props.titleValue}/>
+            {
+                props.collapsed && <AccordionBody/>
+            }
+        </>
+    )
+}
+
+interface AccordionTitleProps {
+    title: string
+}
+
+function AccordionTitle(props: AccordionTitleProps) {
+
+    return <h3>-- {props.title} --</h3>
+}
+
+function AccordionBody() {
+    return (
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    )
+}
